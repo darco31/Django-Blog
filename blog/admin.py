2a5712bc9a187feafff62cb18bb1652c9243103a
Stephen <sdarcy29@hotmail.com>
2022-02-11 17:45:14 +0000

    Update the admin panael witht a list filter and prepopulated slug fields

--- blog/admin.py
+++ blog/admin.py
@@ -5,8 +5,11 @@ from django_summernote.admin import SummernoteModelAdmin
 
 
 @admin.register(Post)
+
 class PostAdmin(SummernoteModelAdmin):
+    """Add posts to the admin panel"""
 
+
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('status', 'created_on')
     summernote_fields = ('content',)
-
-
